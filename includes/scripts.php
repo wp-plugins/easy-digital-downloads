20c6ee767741c8e5089754ebd449e671aee84fc3
mordauk <mordauk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-01 21:53:01 +0000

    Uploading v1.3.4
    git-svn-id: https://plugins.svn.wordpress.org/easy-digital-downloads/tags/1.3.4@632785 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/scripts.php
+++ includes/scripts.php
@@ -9,6 +9,8 @@
  * @since       1.0
 */
 
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
 
 /**
  * Load Scripts
@@ -40,6 +42,7 @@ function edd_load_scripts() {
 				'no_discount' 				=> __('Please enter a discount code', 'edd'), // blank discount code message
 				'discount_applied' 			=> __('Discount Applied', 'edd'), // discount verified message
 				'no_email' 					=> __('Please enter an email address before applying a discount code', 'edd'),
+				'no_username'				=> __('Please enter a username before applying a discount code', 'edd'),
 				'position_in_cart' 			=> isset( $position ) ? $position : -1,
 				'already_in_cart_message' 	=> __('You have already added this item to your cart', 'edd'), // item already in the cart message
 				'empty_cart_message' 		=> __('Your cart is empty', 'edd'), // item already in the cart message
@@ -86,10 +89,10 @@ function edd_register_styles() {
 
 	$file = 'edd.css';
 
-	if ( file_exists( trailingslashit( get_stylesheet_directory() ) . $file ) ) {
-		$url = trailingslashit( get_stylesheet_directory_uri() ) . $file;
-	} elseif ( file_exists( trailingslashit( get_template_directory() ) . $file ) ) {
-		$url = trailingslashit( get_template_directory_uri() ) . $file;
+	if ( file_exists( trailingslashit( get_stylesheet_directory() ) . 'edd_templates/' . $file ) ) {
+		$url = trailingslashit( get_stylesheet_directory_uri() ) . 'edd_templates/' . $file;
+	} elseif ( file_exists( trailingslashit( get_template_directory() ) . 'edd_templates/' . $file ) ) {
+		$url = trailingslashit( get_template_directory_uri() ) . 'edd_templates/' . $file;
 	} elseif ( file_exists( trailingslashit( edd_get_templates_dir() ) . $file ) ) {
 		$url = trailingslashit( edd_get_templates_url() ) . $file;
 	}
